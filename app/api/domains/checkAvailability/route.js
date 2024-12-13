dd625a298fe37b4c6292138fa29f573b15bf894a
Ilyas Shahi <ilyasm.shahi359@gmail.com>
2024-12-13 20:54:33 +0500

    name api cors

--- app/api/domains/checkAvailability/route.js
+++ app/api/domains/checkAvailability/route.js
@@ -1,21 +1,30 @@
 import { NextResponse } from "next/server";
 
-const apiUsername = "quikwebsites";
-const apiToken = "887dc9b929b297771e16ffa54bab1c98ff553930";
-const apiUrl = "https://api.name.com/v4/domains:checkAvailability";
+const corsHeaders = {
+  "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_WEBFLOW_URL,
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
 export async function POST(request) {
   const { domain } = await request.json();
 
   try {
-    const response = await fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        Authorization: "Basic " + btoa(`${apiUsername}:${apiToken}`),
-        "Content-Type": "application/json",
+    const response = await fetch(
+      "https://api.name.com/v4/domains:checkAvailability",
+      {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Basic " +
+            btoa(
+              `${process.env.NAME_API_USERNAME}:${process.env.NAME_API_TOKEN}`,
+            ),
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ domainNames: [domain] }),
       },
-      body: JSON.stringify({ domainNames: [domain] }),
-    });
+    );
     const result = await response.json();
 
     if (!response.ok)
@@ -24,8 +33,18 @@ export async function POST(request) {
         { status: 500 },
       );
 
-    return NextResponse.json(result);
+    return NextResponse.json(result, {
+      status: 200,
+      headers: corsHeaders,
+    });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
+}
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
 }
